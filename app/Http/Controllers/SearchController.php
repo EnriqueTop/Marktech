64f5ab12f672c2d62667fdb8c3a24b756e1fe635
Arturo Herrera <66655201+Artur16211@users.noreply.github.com>
2022-07-24 17:32:02 -0500

    Add files via upload

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Trademarks;
 use Request;
 
 class SearchController extends Controller
 {
     public static function index()
     {
+        $viewData = [];
+        $viewData["trademarks"] = Trademarks::all();
 
         $barra = Request::get('barra');
         $product = Product::where('name', 'LIKE', '%' . $barra . '%')->get();
         if (count($product) > 0) {
-            return view('search.search')->withDetails($product)->withQuery($barra);
+            return view('search.search')->withDetails($product)->withQuery($barra)->with("viewData", $viewData);
         } else {
             return view('search.nofound')->withMessage('No se encontraron resultados para la busqueda');
         }
@@ -22,16 +25,50 @@ class SearchController extends Controller
 
     public static function search()
     {
+        $viewData = [];
+        $viewData["trademarks"] = Trademarks::all();
+
         $barra = Request::get('barra');
         // Sets the parameters from the get request to the variables.
         $trademark = Request::get('trademark');
+        $price = Request::get('price');
 
         // Perform the query using Query Builder
-        if ($trademark == 'all') {
-            $result = Product::where('name', 'LIKE', '%' . $barra . '%')->get();
-        } else {
-            $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('trademark', '=', $trademark)->get();
+        // if ($trademark == 'all' && $price == '0-500') {
+        //     $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '<', 500)->get();
+        // } elseif ($trademark == 'all') {
+        // } else {
+        //     $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('trademark', '=', $trademark)->get();
+        // }
+
+        switch ($trademark) {
+            case 'all' && $price == '1000-2000':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 1000)->where('price', '<', 2000)->where('trademark', '=', $trademark)->orWhere('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 1000)->where('price', '<', 2000)->get();
+                break;
+            case 'all' && $price == '2000-3000':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 2000)->where('price', '<', 3000)->where('trademark', '=', $trademark)->orWhere('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 2000)->where('price', '<', 3000)->get();
+                break;
+            case 'all' && $price == '3000-4000':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 3000)->where('price', '<', 4000)->where('trademark', '=', $trademark)->orWhere('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 3000)->where('price', '<', 4000)->get();
+                break;
+            case 'all' && $price == '4000-5000':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 4000)->where('price', '<', 5000)->where('trademark', '=', $trademark)->orWhere('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 4000)->where('price', '<', 5000)->get();
+                break;
+            case 'all' && $price == '5000-...':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 5000)->where('trademark', '=', $trademark)->orWhere('name', 'LIKE', '%' . $barra . '%')->where('price', '>', 5000)->get();
+                break;
+            case 'all':
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->get();
+                break;
+            default:
+                $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('trademark', '=', $trademark)->get();
+                break;
         }
+
+        // if ($price < 0) {
+        //     $result = Product::where('name', 'LIKE', '%' . $barra . '%')->where('price', '<', $price)->get();
+        // } else {
+        // }
 
         $sort = Request::get('sort');
         // // sort the results by price
@@ -63,9 +100,9 @@ class SearchController extends Controller
         }
         // If there are no results, return a message.
         if (count($result) > 0) {
-            return view('search.search')->withDetails($result)->withQuery($barra)->withSort($sort);
+            return view('search.search')->withDetails($result)->withQuery($barra)->withSort($sort)->with("viewData", $viewData);
         } else {
-            return view('search.nofound')->withMessage('No se encontraron resultados para la busqueda');
+            return view('search.nofound')->withMessage('No se encontraron resultados para la busqueda')->with("viewData", $viewData);
         }
 
     }
