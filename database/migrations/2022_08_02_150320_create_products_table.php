6152b8ff27dcdb3ad340de751633d48620eeac90
Arturo Herrera <66655201+Artur16211@users.noreply.github.com>
2022-08-02 15:04:42 -0500

    Add files via upload

---
+++ database/migrations/2022_08_02_150320_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('discounted_price')->default(0);
+            $table->timestamps();
+            $table->string('category')->nullable();
+            $table->string('subcategory')->nullable();
+            $table->integer('featured')->nullable()->default(0);
+            $table->string('trademark')->default('sin marca');
+            $table->integer('stock')->default(100);
+            $table->integer('sales')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
