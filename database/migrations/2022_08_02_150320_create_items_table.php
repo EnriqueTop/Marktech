4c45096ea81f50dc2826d53a22bcc14bd39e1f38
Arturo Herrera <66655201+Artur16211@users.noreply.github.com>
2022-08-04 00:03:06 -0500

    Add files via upload

---
+++ database/migrations/2022_08_02_150320_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('discounted_price')->default(0);
+            $table->integer('total')->default(0);
+            $table->unsignedBigInteger('order_id')->index('items_order_id_foreign');
+            $table->unsignedBigInteger('product_id')->index('items_product_id_foreign');
+            $table->timestamps();
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
